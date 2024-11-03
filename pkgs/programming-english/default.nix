28678ce14b8814648865b8a9dfabd83978c8ae75
Comamoca <comamoca.dev@gmail.com>
2024-11-03 11:58:00 +0900

    :art: (programming-english/) Formatting

--- pkgs/programming-english/default.nix
+++ pkgs/programming-english/default.nix
@@ -1,44 +1,46 @@
-{ stdenv,
+{
+  stdenv,
   fetchFromGitHub,
   fontforge,
-  makeWrapper, }:
+  makeWrapper,
+}:
 stdenv.mkDerivation {
-            pname = "hello";
-            version = "0.1.0";
-            src = pkgs.fetchFromGitHub {
-              owner = "MatsumotoDesuyo";
-              repo = "programming-english";
-              rev = "main";
-              hash = "sha256-PZRJqDMfy4F92i10jeUY0R5P45YYBvEB3hm55dSbubo=";
-            };
+  pname = "hello";
+  version = "0.1.0";
+  src = pkgs.fetchFromGitHub {
+    owner = "MatsumotoDesuyo";
+    repo = "programming-english";
+    rev = "main";
+    hash = "sha256-PZRJqDMfy4F92i10jeUY0R5P45YYBvEB3hm55dSbubo=";
+  };
 
-	    buildInputs = with pkgs; [ xsv ];
+  buildInputs = with pkgs; [ xsv ];
 
-            buildPhase = ''
-	    xsv cat rows $src/csv/1_Basic300.csv \
-                         $src/csv/2_Advanced.csv \
-                         $src/csv/3_Premise_English100.csv \
-                         > merged.csv
+  buildPhase = ''
+    	    xsv cat rows $src/csv/1_Basic300.csv \
+                             $src/csv/2_Advanced.csv \
+                             $src/csv/3_Premise_English100.csv \
+                             > merged.csv
 
-	    xsv select name merged.csv > words.csv
-	    xsv select name $src/csv/4_Abbreviation70.csv >> words.csv
-	    xsv select name $src/csv/5_\ Acrony.csv >> words.csv
-	    xsv select name $src/csv/6_Reserved.csv >> words.csv
-            '';
+    	    xsv select name merged.csv > words.csv
+    	    xsv select name $src/csv/4_Abbreviation70.csv >> words.csv
+    	    xsv select name $src/csv/5_\ Acrony.csv >> words.csv
+    	    xsv select name $src/csv/6_Reserved.csv >> words.csv
+  '';
 
-	    installPhase = ''
-	      mkdir -p $out/share/dict/csv
-	      mkdir -p $out/share/dict/json
+  installPhase = ''
+    	      mkdir -p $out/share/dict/csv
+    	      mkdir -p $out/share/dict/json
 
-              cp words.csv $out/share/dict/csv/words.csv
+                  cp words.csv $out/share/dict/csv/words.csv
 
-	      cp -r $src/csv $out/share/dict/
-	      cp -r $src/csv $out/share/dict/json
-	    '';
+    	      cp -r $src/csv $out/share/dict/
+    	      cp -r $src/csv $out/share/dict/json
+    	    '';
 
-	    meta = prevAttrs.meta // {
-	      description = "プログラミング英語検定学習に使用するデータをいろいろ詰め合わせ";
-	      homepage = "https://github.com/MatsumotoDesuyo/programming-english";
-	      platforms = lib.platforms.all;
-	    };
-          };
+  meta = prevAttrs.meta // {
+    description = "プログラミング英語検定学習に使用するデータをいろいろ詰め合わせ";
+    homepage = "https://github.com/MatsumotoDesuyo/programming-english";
+    platforms = lib.platforms.all;
+  };
+}
