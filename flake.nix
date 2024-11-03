4b55acf65aba4522fcf4ab56bdfbe4a784ebb5a6
Comamoca <comamoca.dev@gmail.com>
2024-11-03 20:07:22 +0900

    :package: (flake.nix) Add update readme command

--- flake.nix
+++ flake.nix
@@ -40,8 +40,51 @@
           pkgs = import nixpkgs { inherit system; };
         }
       );
+
       packages = forAllSystems (
         system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system}
       );
+
+      devShells = forAllSystems (system: {
+        default =
+          let
+            pkgs = import nixpkgs { inherit system; };
+
+            update-pkgsinfo = pkgs.writeShellScriptBin "update-pkgsinfo" ''
+              nix eval --json .#packages.x86_64-linux --apply 'pkgs:
+                builtins.listToAttrs (
+                  builtins.map (pkgName:
+                    { name = pkgName;
+                      value = {
+                        name = pkgs.''${pkgName}.meta.name or "N/A";
+                        description = pkgs.''${pkgName}.meta.description or "N/A";
+                        homepage = pkgs.''${pkgName}.meta.homepage or "N/A";
+                        license = pkgs.''${pkgName}.meta.license.shortName or "N/A";
+                        platforms = builtins.filter (
+                          x: x == "x86_64-linux" || x == "aarch64-linux" || x == "x86_64-darwin" || x == "aarch64-darwin"
+                        ) (pkgs.''${pkgName}.meta.platforms or []);
+                      };
+                    }
+                  ) (builtins.attrNames pkgs)
+                )' > pkgsinfo.json
+
+              cat pkgsinfo.json | jq -r '
+                "Name|Description|Homepage|License|Platforms|",
+                "----|-----------|--------|-------|---------|",
+                (.[] |  [.name, .description, .homepage, .license, (.platforms | join(", "))] | join("|"))
+                | @text' > pkgsinfo
+
+              sed "/{PACKAGE_LIST}/ {
+                  r pkgsinfo
+                  d
+              }" README.md.tmpl > README.md
+
+	      rm pkgsinfo
+	    '';
+          in
+          pkgs.mkShellNoCC {
+            packages = [ update-pkgsinfo ];
+          };
+      });
     };
 }
