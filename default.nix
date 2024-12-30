c2d35f7c1b6f5d70dc09d956409880a503200616
Comamoca <comamoca.dev@gmail.com>
2024-12-30 19:43:44 +0900

    :sparkles: (default.nix) Update

--- default.nix
+++ default.nix
@@ -18,7 +18,7 @@
 
   programming-english = pkgs.callPackage ./pkgs/programming-english { };
   skk-jisyo-imasparql = pkgs.callPackage ./pkgs/skk-jisyo-imasparql { };
-  nano-tools = pkgs.callPackage ./pkgs/nano-tools { };
+  nano-tools = pkgs.callPackage ./pkgs/nano-tools { inherit pkgs; };
   # secretlint = pkgs.callPackage ./pkgs/secretlint { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
