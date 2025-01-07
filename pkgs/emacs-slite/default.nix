b68f15dfb265e771b300d6c14e79a7d5becea1e3
Comamoca <comamoca.dev@gmail.com>
2025-01-08 05:06:50 +0900

    :sparkles: (pkgs/emacs-slite) Add slite emacs package

---
+++ pkgs/emacs-slite/default.nix
@@ -0,0 +1,24 @@
+{ pkgs }:
+let
+  stdenv = pkgs.stdenv;
+
+  slite = pkgs.emacsPackages.melpaBuild {
+    pname = "slite";
+    version = "1";
+    buildInputs = [ ];
+
+    src = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/tdrhq/slite/refs/heads/main/slite.el";
+      hash = "sha256-ohVZUSpHNDLna6BmFtgtUSKV7Pj6Nj+67imVWXWn+mQ=";
+    };
+  };
+in 
+{
+  inherit slite;
+
+  meta = {
+    description = "a SLIme-based TEst runner for FiveAM and Parachute Tests";
+    homepage = "https://github.com/tdrhq/slite";
+    platforms = pkgs.lib.platforms.all;
+  };
+}
